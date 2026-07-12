5819b43d9f323ec0844a2314aef37be4f4de826b
adrouin <amaury.d.pro@proton.me>
2026-07-12 22:28:47 +0200

    Spider : Adding exception policy and manage -l / -r

--- Spider/Spider.py
+++ Spider/Spider.py
@@ -5,9 +5,13 @@ def spider():
     parser = argparse.ArgumentParser()
     parser.add_argument("url", help="url from the website you want to extract images")
     parser.add_argument("-r", help="recursively downloads the images", action="store_true")
-    parser.add_argument("-l", "--level", type=int, default=5)
+    parser.add_argument("-l", "--level", type=int, default=None)
     parser.add_argument("-p", "--path", type=str, default="./data/")
     args = parser.parse_args()
+    if args.level is not None and not args.r:
+        parser.error("-l need to be used with -r")
+    elif args.r and args.level is None:
+        args.level = 5
     print(vars(args))
 
 
