86601209b8efe55b2e9bd943060e79cb664189f4
adrouin <amaury.d.pro@proton.me>
2026-07-13 20:08:12 +0200

    URl fetching principe is on, still need to check url format

--- Spider/Spider.py
+++ Spider/Spider.py
@@ -1,7 +1,20 @@
 import argparse
+import urllib.request, urllib.error
+
+def fetch_page(url : str):
+    try:
+        with urllib.request.urlopen(url, timeout = 10) as response:
+            html = response.read().decode()
+        return html
+    except urllib.error.HTTPError:
+        print("Error HTTP")
+        return None
+    except urllib.error.URLError:
+        print("Url error")
+        return None
 
 
-def spider():
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("url", help="url from the website you want to extract images")
     parser.add_argument("-r", help="recursively downloads the images", action="store_true")
@@ -12,7 +25,14 @@ def spider():
         parser.error("-l need to be used with -r")
     elif args.r and args.level is None:
         args.level = 5
-    print(vars(args))
+    return args
+
+
+def spider():
+    args = parse_args()
+    html = fetch_page(args.url)
+    print(html)
+
 
 
 if __name__ == "__main__":
